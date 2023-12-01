9e09a59654137f765b68940b8385bb22ccd0ec3d
iamaryanyadavv <aryanyadav1601@gmail.com>
2023-12-01 18:56:57 +0530

    added navbar and no-wallet pages

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./header.css";
+import { Navbar, Link, Image, Popover, Text } from "@nextui-org/react";
+import Logo from '../../assets/logo.png'
+import Bitcoin from '../../assets/bitcoin.png'
+
+function Header() {
+
+    const items = [
+        { name: 'Ashonk', href: '/' },
+        { name: 'Raffle', href: '/ashonkraffle' },
+    ]
+
+    var active = window.location.pathname
+
+    return (
+        <Navbar className="navbar" variant="static" shouldHideOnScroll={false}>
+            <Navbar.Toggle color="inherit" showIn="sm" />
+            <Navbar.Brand hideIn="sm" css={{ '&:hover': { transform: 'scale(1.25)' } }}>
+                <Image css={{
+                    width: '40px',
+                    height: '40px',
+                    transitionDuration: '0.5s',
+                    transitionProperty: 'transform',
+                    '&:hover': {
+                        cursor: 'pointer',
+                        transform: 'rotate(360deg)',
+                    }
+                }} src={Logo}
+                    onClick={() => {
+                        window.location.pathname = ''
+                    }} />
+            </Navbar.Brand>
+            <Navbar.Content hideIn="sm" variant="highlight-rounded">
+                {items.map((item, index) => {
+                    return (
+                        <Navbar.Link key={index} isActive={item.href === active.substring(0, item.href.length + 1)} href={item.href}
+                            css={{
+                                // fontFamily: 'MEregular'
+                            }}>
+                            {item.name}
+                        </Navbar.Link>
+                    )
+                })}
+
+            </Navbar.Content>
+            <Navbar.Content showIn={'sm'}>
+                <Image css={{
+                    width: '40px',
+                    height: '40px'
+                }} src={Logo} />
+            </Navbar.Content>
+            <Navbar.Content>
+                <Link target="_blank" href="https://bitcoin.org/en/">
+                    <Image css={{
+                        width: '40px',
+                        height: '40px'
+                    }} src={Bitcoin} />
+                </Link>
+            </Navbar.Content>
+
+
+            <Navbar.Collapse showIn={"sm"}>
+                {items.map((item, index) => {
+                    return (
+                        <Navbar.CollapseItem key={index} isActive={item.href === active.substring(0, item.href.length + 1)}>
+                            <Link href={item.href}
+                                css={{
+                                    minWidth: "100%",
+                                }}
+                            >
+                                {item.name}
+                            </Link>
+                        </Navbar.CollapseItem>)
+                })}
+
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
---
+++ frontend/src/pages/buyToken.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid, Text, Image, Row, Col } from "@nextui-org/react";
+
+export default function BuyToken () {
+    return(
+        <>
+            buy our Token pls
+        </>
+    )
+}
