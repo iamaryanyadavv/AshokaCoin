added67caec784f8162c524ec63bf968f079f1c9
Samvit Jatia <samvit@hotmail.com>
2023-12-04 20:49:31 +0530

    adding updated builds

--- coin/truffle-config.js
+++ coin/truffle-config.js
@@ -8,11 +8,11 @@ module.exports = {
   },
 
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: "7545",
-      network_id: "*", // match any network id
-    },
+    //   development: {
+    //     host: "127.0.0.1",
+    //     port: "7545",
+    //     network_id: "*", // match any network id
+    //   },
     goerli: {
       provider: () =>
         new HDWalletProvider(
